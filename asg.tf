fb9f4bb3e4b824d1dd828584ad7a4cffcd2d97ff
Tim Gibbon <tng@chegwin.org>
2021-07-16 20:43:45 +0100

    Purposefully misalign

--- asg.tf
+++ asg.tf
@@ -51,7 +51,7 @@ resource "aws_autoscaling_group" "asg" {
 resource "aws_autoscaling_policy" "web_policy_up" {
   name                   = "web_policy_up"
   scaling_adjustment     = 1
-  adjustment_type        = "ChangeInCapacity"
+  adjustment_type         = "ChangeInCapacity"
   cooldown               = 300
   autoscaling_group_name = aws_autoscaling_group.asg.name
 }
