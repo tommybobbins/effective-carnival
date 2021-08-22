f817fc1741931f8aa020f7f81b06d861dbf19c25
Github auto formatter <autoformat@users.noreply.github.com>
2021-08-22 20:30:03 +0000

    Terraform formatting

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 # Variable declarations
 variable "aws_region" {
-  description    = "AWS region"
-  type                        = string
+  description = "AWS region"
+  type        = string
   default     = "us-east-1"
 }
 
@@ -12,7 +12,7 @@ variable "project_name" {
 
 variable "Hostname" {
   description = "Hostname of the server which will be created"
-  default           = "instance-bobbins1"
+  default     = "instance-bobbins1"
 }
 
 variable "bucket_seed" {
--- vpc.tf
+++ vpc.tf
@@ -4,11 +4,11 @@ module "vpc" {
   version = "2.64.0"
 
   cidr            = var.vpc_cidr_block
-  azs              = data.aws_availability_zones.available.names
-  private_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   # Need nat gateway if ec2 instances are in private subnet
-  enable_nat_gateway  = true
+  enable_nat_gateway = true
   #enable_nat_gateway = false
   enable_vpn_gateway = false
 
