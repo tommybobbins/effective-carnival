b5d4b3e3b368647bde4d024e6f4ee5bc92c335de
Tim <tim.gibbon@peak.ai>
2021-09-20 08:10:45 +0100

    Misalign the vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   version = "2.64.0"
 
   cidr            = var.vpc_cidr_block
-  azs             = data.aws_availability_zones.available.names
+  azs                  = data.aws_availability_zones.available.names
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   # Need nat gateway if ec2 instances are in private subnet
